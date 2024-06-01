620706e6e172f3d38a5acdc5904398e7bbd5beb4
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-06-02 03:03:13 +0530

    school icon added

--- src/components/EducationContainer.jsx
+++ src/components/EducationContainer.jsx
@@ -9,7 +9,7 @@ const EducationContainer = () => {
   return (
     <div className='mt-14 '>
       <div className='flex items-center justify-between pl-2 pr-2'>
-        <h1 className='text-sm' ><i className='bx bxs-briefcase align-middle'></i> Education</h1>
+        <h1 className='text-sm' ><i className='bx bxs-school align-middle'></i> Education</h1>
         <p className='text-xs '><a href='https://linkedin.com/in/thedeepakmaurya'>View all </a><i className='bx bx-link-external align-middle'></i></p>
       </div>
       <div className='flex flex-wrap '>

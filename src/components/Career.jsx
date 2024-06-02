ccb9173c0a3134d7acde6940732651bd52ec41fb
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-06-02 10:26:57 +0530

    resize img

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -7,7 +7,7 @@ const Career = ({ career }) => {
     return (
         <div className='w-1/2 flex gap-2 mt-8'>
             <div>
-                <a href='https://linkedin.com/in/thedeepakmaurya' ><img className='w-[100px] border-white shadow-md shadow-black rounded-lg hover:grayscale' alt='thumbnail-img' src={img} /></a>
+                <a href='https://linkedin.com/in/thedeepakmaurya' ><img className='w-[80px] border-white shadow-md shadow-black rounded-lg hover:grayscale' alt='thumbnail-img' src={img} /></a>
             </div>
             <div className=''>
                 <h1 className='text-md font-medium'>{company} <span className='text-xs pb-1'>{designation}</span></h1>
--- src/components/Certification.jsx
+++ src/components/Certification.jsx
@@ -7,7 +7,7 @@ const Certificaion = ({ certification }) => {
     return (
         <div className='w-1/2 flex gap-2 mt-8'>
             <div>
-                <a href='https://linkedin.com/in/thedeepakmaurya' ><img className='w-[56px] border-white shadow-md shadow-black rounded-lg hover:grayscale' alt='thumbnail-img' src={img} /></a>
+                <a href='https://linkedin.com/in/thedeepakmaurya' ><img className='w-[46px] border-white shadow-md shadow-black rounded-lg hover:grayscale' alt='thumbnail-img' src={img} /></a>
             </div>
             <div className=''>
                 <h1 className='text-md font-medium'>{institute}</h1>

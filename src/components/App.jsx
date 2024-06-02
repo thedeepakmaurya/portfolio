a556f7730cfeb302ee454a9d57aff5f20ce63e78
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-06-02 20:27:03 +0530

    media query fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,7 @@ import { info } from "../utils/userContext";
 function App() {
   return (
     <userContext.Provider value={info}>
-      <div className=" sm:ml-5 sm:mr-5 sm:mt-10 sm:mb-10 md:ml-52 md:mr-52 md:mt-5 md:mb-10 xl:ml-96 xl:mr-96 xl:mt-10 xl:mb-10">
+      <div className=" sm:ml-5 sm:mr-5 sm:mt-10 sm:mb-10 md:ml-20 md:mr-20 md:mt-10 md:mb-10 lg:ml-40 lg:mr-40 lg:mt-10 lg:mb-10 xl:ml-96 xl:mr-96 xl:mt-10 xl:mb-10">
         <Profile />
         <StackContainer />
         <ProjectContainer />

c10a51e9cc858645c6a99cd7654c878d28ec642f
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-06-07 00:53:11 +0530

    tab layout fixed

--- src/components/CareerContainer.jsx
+++ src/components/CareerContainer.jsx
@@ -9,7 +9,7 @@ const CareerContainer = () => {
   const { careerdesc } = useContext(userContext);
 
   return (
-    <div className='mt-16 sm:w-[90%] w-[43%]'>
+    <div className='mt-16 sm:w-[90%] md:w-[70%] w-[43%]'>
       <div className='flex items-center justify-between pl-2 pr-2'>
         <h1 className='font-medium text-sm' ><i className='bx bxs-briefcase align-middle'></i> Career</h1>
         <p className='font-medium text-xs underline decoration-dotted'><a href='https://linkedin.com/in/thedeepakmaurya'>View all </a></p>
--- src/components/CertificationContainer.jsx
+++ src/components/CertificationContainer.jsx
@@ -7,7 +7,7 @@ const CertificationContainer = () => {
   const { certification } = INFO;
 
   return (
-    <div className='mt-16 sm:w-[90%] w-[43%]'>
+    <div className='mt-16 sm:w-[90%] md:w-[70%] w-[43%]'>
       <div className='flex items-center justify-between pl-2 pr-2'>
         <h1 className='text-sm font-medium' ><i className='bx bxs-notepad align-middle'></i> Certification</h1>
         <p className='text-xs font-medium underline decoration-dotted'><a href='https://linkedin.com/in/thedeepakmaurya'>View all </a></p>

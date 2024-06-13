778327b990cdfc92937b210ab7b31db069811fd0
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-06-14 01:55:15 +0530

    config update

--- src/utils/userContext.jsx
+++ src/utils/userContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 export const info = {
     profile: {
         name: 'Deepak Maurya',
-        summary: 'React JS enthusiast with a sharp attention to detail and a passion for developing smooth web applications. Eager to immerse myself in front-end development, I am actively seeking an internship opportunity to further explore and hone my skills in this exciting field.',
+        summary: 'React JS enthusiast with a sharp attention to detail and a passion for developing smooth web applications. Eager to immerse myself in web development, As an Intern at Unified Mentor, I am currently working on two key projects: the Student-Teacher Booking Appointment system and the Catering Reservation and Ordering System.',
     },
     careerdesc: {
         description: 'As a Project Delivery Executive played a pivotal role in overseeing and ensuring the successful delivery of projects. ',

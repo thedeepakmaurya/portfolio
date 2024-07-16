bc40c23d0b5b3a2444787dc55538d3b2dc1380f1
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-07-16 11:53:47 +0530

    layout fix

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -5,11 +5,11 @@ const Stack = ({ stack }) => {
     const {  description, name, img } = stack;
 
     return (
-        <div className='w-1/2 flex gap-2 mt-8'>
-            <div>
+        <div className='w-1/2 flex mt-8'>
+            <div className='w-1/6 sm:w-1/3'>
                 <img className='w-[46px] h-[46px] border-white shadow-md shadow-black rounded-lg hover:grayscale' alt='thumbnail-img' src={img} />
             </div>
-            <div className=''>
+            <div className='w-5/6 sm:w-2/3'>
                 <h1 className='text-md font-medium'>{name}</h1>
                 <p className='text-sm pb-1'>{description}</p>
             </div>
